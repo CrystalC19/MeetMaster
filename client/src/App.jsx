dcb021eeab4d74f08cfa07b8f8ff6f1f336273d9
Jerrika Gabriel <jerrikagabriel@Jerrikas-Air.attlocal.net>
2024-08-07 20:42:17 -0500

    updated all the port to 3001.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { Outlet, useLocation } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import { loadStripe } from "@stripe/stripe-js";
 import { Elements, PaymentElement } from "@stripe/react-stripe-js";
 import Header from './components/header';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
-import CheckoutForm from "./CheckoutForm";
 import './App.css';
 import React, { useState, useEffect } from "react";
 
