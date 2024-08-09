ab3f68f5de00f1d8b53f636e7c8bac3c89d3c1e6
Jerrika Gabriel <jerrikagabriel@Jerrikas-Air.attlocal.net>
2024-08-08 20:55:06 -0500

    trying to install needed packages.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
+import '../src/';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/home/index.jsx';
 
