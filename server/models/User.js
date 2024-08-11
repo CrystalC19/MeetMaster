baa7ca8df5f916f76a2e57213c2fae62ae25d4a9
Jerrika Gabriel <jerrikagabriel@Jerrikas-Air.attlocal.net>
2024-08-10 21:41:44 -0500

    updated models and schemas to work with the payment.

--- server/models/User.js
+++ server/models/User.js
@@ -2,11 +2,15 @@
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const Order = require('./Order');
+
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    orders: [Order.schema]
+    
 });
 
 // Hash the password before saving the user
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,29 @@
 const typeDefs = `
+    type Category {
+        _id: ID
+        name: String
+    }
+
+    type Product {
+        _id: ID
+        name: String
+        description: String
+        image: String
+        quantity: Int
+        price: Float
+        category: Category
+    }
+
+    type Order {
+        _id: ID
+        purchaseDate: String
+        products: [Product]
+        orders: [Order]
+  }
+    
+    type Checkout {
+    session: ID
+  }
     type User {
         _id: ID
         name: String
@@ -6,10 +31,19 @@ const typeDefs = `
     type Query {
         users: [User]!
         user(userId: ID!): User
+        categories: [Category]
+        products(category: ID, name: String): [Product]
+        product(_id: ID!): Product
+        order(_id: ID!): Order
+        checkout(products: [ProductInput]): Checkout
+
     }
     type Mutation {
         addUser(name: String!): User
         removeUser(userId: ID!): User
+        addOrder(products: [ID]!): Order
+        updateUser(firstName: String, lastName: String, email: String, password: String): User
+        updateProduct(_id: ID!, quantity: Int!): Product
     }
 `;
 
