ab572c0e7df9f09588e32ecd2bf995d1427d8be8
Jerrika Gabriel <jerrikagabriel@Jerrikas-Air.attlocal.net>
2024-08-10 22:23:22 -0500

    added queries for the products, checkout, and categories.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,55 @@
 import { gql } from '@apollo/client';
 
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ProductInput]) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      quantity
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+
 export const GET_USERS = gql`
   users {
     _id
