d65fc2d96734a5dd798dae4ded7a0925fe356bb9
crystal <cristalcruz19@gmail.com>
2024-08-14 19:07:40 -0500

    mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/meetMaster');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/meetMaster');
 
 module.exports = mongoose.connection;
